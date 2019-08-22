dd173eb57a156a52ab47fd20d17ca888f2f03e10
myGitZzb <965889515@qq.com>
2019-08-22 13:10:45 +0800

    二次Git

--- src/com/pccw/demo/Test.java
+++ src/com/pccw/demo/Test.java
@@ -3,5 +3,6 @@ package com.pccw.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是第一个Test类");
+        System.out.println("这是测试Git的");
     }
 }
